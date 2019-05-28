5bd75edd5fcf475aeb71c294070e595fe1bfe511
Marcosjrsilva <marcosjrsilva1@gmail.com>
2019-05-28 15:15:32 -0300

    Encerrado conta

--- src/aula05/Aula05.java
+++ src/aula05/Aula05.java
@@ -27,8 +27,11 @@ public class Aula05 {
         p2.abrirConta("CP");
         
         p1.depositar(100);
-        p2.depositar(350);
+        p2.depositar(500);
         
+        p2.sacar(100);
+        p1.sacar(150);
+        p1.fecharConta();
         
         p1.estadoAtual();
         p2.estadoAtual();
