75c155ee70dc64a718a80ea557814d485b03b6e4
Marcosjrsilva <marcosjrsilva1@gmail.com>
2019-05-28 09:31:17 -0300

    Criado métodos especias, construtor, get e set.

--- src/aula05/ContaBanco.java
+++ src/aula05/ContaBanco.java
@@ -40,4 +40,53 @@ public class ContaBanco {
     public void pagarMesal(){
     
     }
+    
+    // **** MÉTODOS ESPECIAS ******
+
+    public ContaBanco() {
+        this.saldo = 0;
+        this.status = false;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
 }
